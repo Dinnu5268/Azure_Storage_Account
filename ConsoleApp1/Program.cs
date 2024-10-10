6422631c4eea1d8986d3884fa8ca8bc86d962f66
dinnu5268 <dinesh.puvvala.dp@gmail.com>
2024-10-10 11:16:52 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Storage.Blobs;
+using System;
+
+namespace BlobStorage
+{
+    class Program
+    {
+        static string connectionString = "DefaultEndpointsProtocol=https;AccountName=demostacc001;AccountKey=8LFS7aBu1bQHe5qzF6SfG+h2ZMuqnd8qCTOgrA7vIhglSa6BILolvB+HUf45IBmF4MDb+5ME0clu+ASt8MWkaA==;EndpointSuffix=core.windows.net";
+        static string ContainerName = "demo";
+        static void Main(string[] args)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(ContainerName);
+
+            var blobs = containerClient.GetBlobs();
+            foreach (var blob in blobs)
+            {
+                Console.WriteLine(blob.Name);
+                BlobClient blobclient = containerClient.GetBlobClient(blob.Name);
+                blobclient.DownloadTo(@"D:\AzureStorageAccount\" +  blob.Name);
+            }
+            Console.Read();
+        }
+    }
+}
